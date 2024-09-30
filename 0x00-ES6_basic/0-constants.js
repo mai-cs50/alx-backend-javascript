3ea10db453300585458a1b7210d52af95e843a59
mai-cs50 <maimohamed.1992@yahoo.com>
2024-09-30 11:40:37 -0700

    r

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,14 @@
-// Use const for taskFirst
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
   }
   
-  // Use let for taskNext
+  export function getLast() {
+    return ' is okay';
+  }
+  
   export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-  
     return combination;
-  }
-  
-  export function getLast() {
-    return ' is okay';
   }
