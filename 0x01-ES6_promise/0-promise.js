f2b8c85241df3251e01a39c4c9969c4b339192e2
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-03 00:51:02 +0300

    s

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call with setTimeout
+    setTimeout(() => {
+      // For demonstration purposes, resolve the promise
+      resolve("API response data");
+
+      // If there was an error, you could use reject
+      // reject(new Error("API call failed"));
+    }, 2000); // Simulating a 2-second delay
+  });
+}
