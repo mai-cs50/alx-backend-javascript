4851814c018730d7142aeb83d954c58930713ad2
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-03 22:04:03 +0300

    f

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
-"""
-0-main
-"""
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+def pascal_triangle(n):
+    if n <= 0:
+        return []
 
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+    triangle = []
+    
+    for i in range(n):
+        row = [1] * (i + 1)  # Start each row with 1s
+        if i > 1:  # For rows beyond the first two
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
 
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    return triangle
