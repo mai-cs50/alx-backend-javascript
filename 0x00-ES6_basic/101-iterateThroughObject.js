4cb0b8b9f4547f6073d2dddb10e653a559fed8cb
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-04 11:45:05 +0300

    s

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,8 +2,8 @@ export default function iterateThroughObject(reportWithIterator) {
   const employeeNames = [];
 
   for (const employee of reportWithIterator) {
-      employeeNames.push(employee);
+    employeeNames.push(employee);
   }
- 
+
   return employeeNames.join(' | ');
 }
