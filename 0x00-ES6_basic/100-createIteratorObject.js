88f18c70c4377d6ee04eeec6aa4269c492d9f496
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-04 11:41:12 +0300

    s

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,12 @@
 export default function createIteratorObject(report) {
-    const employees = Object.values(report.allEmployees).flat();
-    let index = 0;
-  
-    return {
-      next() {
-        if (index < employees.length) {
-          return { value: employees[index++], done: false };
-        } else {
+  const employees = Object.values(report.allEmployees).flat();
+  let index = 0;
+
+  return {
+    next() {
+      if (index < employees.length) {
+        return { value: employees[index++], done: false };
+      } else {
           return { done: true };
         }
       },
