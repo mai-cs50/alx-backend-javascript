b50c44529940a272ca3670775321fd0f29ec7693
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-05 15:11:06 +0300

    d

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const newtask = true;
-    const newtask2 = false;
-    return [newtask, newtask2];
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
