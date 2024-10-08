4c54881dedb2171b3e8db4f337f4a06531e917b7
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-08 20:27:17 +0300

    d

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,12 +3,12 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   let count = weakMap.get(endpoint) || 0;
-    count += 1;
-    
-    weakMap.set(endpoint, count);
+  count += 1;
+
+  weakMap.set(endpoint, count);
 
 
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
